90c2479d1dd4c1e24c059f0921eef10db0920fe7
meetgami510 <meetgami105@gmail.com>
2023-01-31 20:25:39 +0530

    Making Login and Register Form also fix Some JSON bug

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Upload from "./components/Upload";
 import Login from "./components/Login";
 import Journal from "./components/Journal"
+import Register from "./components/Register";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,6 +15,7 @@ function App() {
             <Route path='/login' element={<Login/>} />   
             <Route path='/senddata' element={<Upload/>} />
             <Route path='/journal' element={<Journal/>} />
+            <Route path='/register' element={<Register/>}/>
         </Routes>
       </Router>
       <ToastContainer/>
