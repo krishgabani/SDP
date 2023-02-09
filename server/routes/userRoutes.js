6fa3f0d10f9afeeb7a53777af4ac4ea7e26d646e
meetgami510 <meetgami105@gmail.com>
2023-02-09 22:41:51 +0530

    simple Registeration End to End Flow Done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {registerUser} = require('../Controllers/registerUser')
 
+
 router.post('/register',registerUser);
 
 module.exports = router;
