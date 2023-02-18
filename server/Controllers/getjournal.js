c2f2160fd69a9e60cb9f05041525300d5a7b4a28
Krish-Gabani <krishgabani7@gmail.com>
2023-02-18 18:06:33 +0530

    now Journal Records will display according to logged user

--- server/Controllers/getjournal.js
+++ server/Controllers/getjournal.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const JournalDataModel = require('../models/journal')
+const express = require("express");
+const JournalDataModel = require("../models/journal");
 
-exports.getjournal = async (req,res) => {
-    console.log(req.body);
-    console.log("hii");
+exports.getjournal = async (req, res) => {
+  let jou = [];
+  console.log(req.body);
+  if (
+    req.body.Designation === "coordinator" ||
+    req.body.Designation === "hod"
+  ) {
+    jou = await JournalDataModel.find({
+      Data_Submitting_Author_department: req.body.Department,
+    });
+  } else if (req.body.Designation === "faculty") {
+    let asFirstAuthor = await JournalDataModel.find({
+      First_Author_name: "Harshadkumar B. Prajapati",
+    });
 
-    let jou= await JournalDataModel.find({Data_Submitting_Author_department : req.body.Department});
-    //console.log(jou);
-    res.send({
-        message: "user is valid",
-        data:jou
-    })
-}
+    let asOtherAuthor = await JournalDataModel.find({
+      Names_of_Other_Author_From_DDU: new RegExp(
+        ".*" + "Harshadkumar B. Prajapati" + ".*"
+      ),
+    });
+
+    jou = [].concat(asFirstAuthor, asOtherAuthor);
+  }
+
+  res.send({
+    message: "user is valid",
+    data: jou,
+  });
+};
