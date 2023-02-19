c169155cbf6c82866dbbd2f902d5dac55b261b19
meetgami510 <meetgami105@gmail.com>
2023-02-19 18:36:29 +0530

    Update Functionlity in backend Done

--- server/Controllers/editjournal.js
+++ server/Controllers/editjournal.js
@@ -5,7 +5,10 @@ exports.editjournal = async (req,res) => {
     console.log("edit");
     console.log(req.body);
 
-    return res.send({
+    const resm = await JournalDataModel.updateMany({Sr_No:req.body.Sr_No},req.body);
+    // const tem = await JournalDataModel.findOne({Sr_No:req.body.Sr_No});
+    // console.log(tem);
+    return res.status(200).send({
         message: "Edited Data Succefully",
     })
 }
