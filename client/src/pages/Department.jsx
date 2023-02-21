fe53d2ce96f41368ee65239dcd226a56dfb48626
meetgami510 <meetgami105@gmail.com>
2023-02-21 09:57:26 +0530

    All Department show in Admin using map

--- client/src/pages/Department.jsx
+++ client/src/pages/Department.jsx
@@ -1,13 +1,14 @@
 import { React, useEffect,useState } from "react";
 import Layout from '../components/Layout/Layout'
 import axios from "axios";
+import "../styles/Department.css"
 
 const Department = () => {
-    const [alldepartment,setAllDepartment] = useState([]);
+    const [departmentList,setDepartmentList] = useState([]);
     const [depart,setdepart] = useState({
         department:""
     });
-
+    const departmentListHtml = departmentList.map((value) => <h3>{value}</h3>)
     const sethandler = (e) => {
         const { name, value } = e.target;
         setdepart({
@@ -19,18 +20,27 @@ const Department = () => {
     useEffect(() => {
         const getAllDepartment = async () => {
             const tem = await axios.post("http://localhost:5000/api/user/getdepartment");
-            console.log(tem.data);  
+            //console.log(tem.data);  
+            let deplist = tem.data.data;
+            let mylist = deplist.map((dep)=>dep.department)
+            setDepartmentList(mylist);
         }
         getAllDepartment();
-    })
+        // if(!departmentList) {
+        //     getAllDepartment();
+        // }
+        
+    },[])
     const editdepartment = async () => {
         const res = await axios.post("http://localhost:5000/api/user/editdepartment",depart);
         console.log(res.data);
     }
+
   return (
     <Layout>
         Add Department <input type="text" name="department" value={depart.department} onChange={sethandler}/>
         <input type="submit" onClick={editdepartment}/> 
+        {departmentListHtml}
     </Layout>
   )
 }
