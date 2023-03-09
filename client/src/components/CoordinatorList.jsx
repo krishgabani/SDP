7220a16afc0896b5185931629b13fabbd4127e00
meetgami510 <meetgami105@gmail.com>
2023-03-09 20:21:17 +0530

    department and coordinate in same page

--- client/src/components/CoordinatorList.jsx
+++ client/src/components/CoordinatorList.jsx
@@ -4,25 +4,36 @@ import {useNavigate} from 'react-router-dom'
 
 const CoordinatorList = ({coordinator}) => {
     const navigate  = useNavigate();
-  return (
-    <>
-        <div className="card m-4" style={{cursor:"pointer"}} onClick={() => navigate(`/allfaculty/${coordinator._id}`)}> 
-            <div className="card-header" >
-                Department : <b>{coordinator.Department}</b>     
+    if(coordinator.email){
+        return (
+            <>
+                <div className="card m-4" style={{cursor:"pointer"}} onClick={() => navigate(`/allfaculty/${coordinator._id}`)}> 
+                    <div className="card-header" >
+                        Department : <b>{coordinator.Department}</b>     
+                    </div>
+                    <div className="card-body">
+                    <p>
+                        <b>Name :</b> {coordinator.name}
+                    </p>
+                    <p>
+                        <b>Email :</b> {coordinator.email}
+                    </p>
+                    <button className='m-2'>View All Faculty</button>
+                    </div>
+                </div>
+            </>
+          )
+    }else{
+        return(
+            <>
+            <div className="card m-4"> 
+                <div className="card-header" >
+                    Department : <b>{coordinator.department}</b>     
+                </div>
             </div>
-            <div className="card-body">
-            <p>
-                <b>Name :</b> {coordinator.name}
-            </p>
-            <p>
-                <b>Email :</b> {coordinator.email}
-            </p>
-            <button className='m-2'>View All Faculty</button>
-            </div>
-        </div>
-    </>
-  )
+            </>
+        )
+    }
 }
-
 export default CoordinatorList
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,38 +4,101 @@ import { useSelector } from "react-redux";
 import "../styles/Home.css";
 import axios from "axios";
 import CoordinatorList from "../components/CoordinatorList";
-import {Row,message} from 'antd'
+import { Row, message } from "antd";
 
 const Home = ({ cookies, removeCookies }) => {
   const { user } = useSelector((state) => state.user);
-  const [coordinatorList,setcoordinatorList] = useState([]);
+  const [coordinatorList, setcoordinatorList] = useState([]);
+  const [departmentList, setDepartmentList] = useState([]);
+  const [depart, setdepart] = useState({
+    department: "",
+  });
   console.log(user);
- 
-  useEffect(()=>{
-    if(user?.Designation === 'Admin') {
-      const getAllCoordinates = async () => {
-        const tem = await axios.get("http://localhost:5000/api/admin/getAllCooridnates");
-        console.log(tem.data);
-        setcoordinatorList(tem.data.data);
-        console.log(coordinatorList);
+
+  const sethandler = (e) => {
+    const { name, value } = e.target;
+    setdepart({
+      ...depart,
+      [name]: value,
+    });
+    console.log(depart);
+  };
+
+  useEffect(() => {
+    try {
+      if (user?.Designation === "Admin") {
+        const getAllCoordinates = async () => {
+          const tem = await axios.get(
+            "http://localhost:5000/api/admin/getAllCooridnates"
+          );
+          console.log(tem.data);
+          setcoordinatorList(tem.data.data);
+          console.log(coordinatorList);
+        };
+
+        getAllCoordinates();
       }
-      
-      getAllCoordinates();
+    } catch (error) {
+      console.log(error);
+      console.log("In User Admin is something wrong");
     }
-  },[])
+  }, []);
+
+  useEffect(() => {
+    try {
+      const getAllDepartment = async () => {
+        const tem = await axios.post(
+          "http://localhost:5000/api/user/getdepartment"
+        );
+        //console.log(tem.data);
+        let deplist = tem.data.data;
+        let mylist = deplist.map((dep) => dep.department);
+        setDepartmentList(mylist);
+      };
+      getAllDepartment();
+    } catch (error) {
+      console.log(error);
+      console.log("Error Occur in Department");
+    }
+  }, []);
+
+  const editdepartment = async () => {
+    try{
+      window. location. reload()
+      const res = await axios.post(
+        "http://localhost:5000/api/user/editdepartment",
+        depart
+      );
+      console.log(res.data);      
+    }catch(error) {
+      console.log(error);
+      console.log("Error in editdepartment ")
+    }
+
+  };
 
   return (
     <Layout removeCookies={removeCookies}>
       <>
-      <h1 className="text-center">All Coordinators</h1>
-      <Row>
-          {coordinatorList && coordinatorList.map((coordinator) => <CoordinatorList coordinator={coordinator} />)}
-      </Row>
-      
+        <h2 className="text-center">All Coordinators</h2>
+        Add Department{" "}
+        <input
+          type="text"
+          name="department"
+          value={depart.department}
+          onChange={sethandler}
+          required
+        />
+        <input type="submit" onClick={editdepartment} />
+        <Row>
+          {coordinatorList &&
+            coordinatorList.map((coordinator) => (
+              <CoordinatorList coordinator={coordinator} />
+            ))}
+        </Row>
       </>
     </Layout>
   );
-
 };
 
 export default Home;
